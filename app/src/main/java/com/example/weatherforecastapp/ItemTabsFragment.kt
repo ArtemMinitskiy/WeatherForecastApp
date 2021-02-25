cf049ccef8f9ff883632efd6e0d09dd53de66cfe
ArtemMinitskiy <rukokuro@gmail.com>
2021-02-25 12:12:21 +0200

    Small fixes

--- app/src/main/java/com/example/weatherforecastapp/ItemTabsFragment.kt
+++ app/src/main/java/com/example/weatherforecastapp/ItemTabsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.weatherforecastapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_tabs.view.*
+import java.util.*
 
 class ItemTabsFragment : Fragment() {
+    val calendar = Calendar.getInstance()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_tabs, container, false)
@@ -18,16 +21,47 @@ class ItemTabsFragment : Fragment() {
 
 //        initTab(tab_layout, pager)
 
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+
         val adapter = context?.let { it -> TabsPagerAdapter(childFragmentManager, it, 4) }
         rootView.viewpager.adapter = adapter
         rootView.tab_layout.setupWithViewPager(rootView.viewpager)
 
         for (i in 0 until rootView.tab_layout.tabCount) {
-            when (i) {
-                0 -> rootView.tab_layout.getTabAt(i)!!.text = "Morning"
-                1 -> rootView.tab_layout.getTabAt(i)!!.text = "Day"
-                2 -> rootView.tab_layout.getTabAt(i)!!.text = "Evening"
-                3 -> rootView.tab_layout.getTabAt(i)!!.text = "Night"
+            when (hour) {
+                in 6..11 -> {
+                    when (i) {
+                        0 -> rootView.tab_layout.getTabAt(i)!!.text = "Morning"
+                        1 -> rootView.tab_layout.getTabAt(i)!!.text = "Day"
+                        2 -> rootView.tab_layout.getTabAt(i)!!.text = "Evening"
+                        3 -> rootView.tab_layout.getTabAt(i)!!.text = "Night"
+                    }
+                }
+                in 12..17 -> {
+                    when (i) {
+                        0 -> rootView.tab_layout.getTabAt(i)!!.text = "Day"
+                        1 -> rootView.tab_layout.getTabAt(i)!!.text = "Evening"
+                        2 -> rootView.tab_layout.getTabAt(i)!!.text = "Night"
+                        3 -> rootView.tab_layout.getTabAt(i)!!.text = "Morning"
+                    }
+                }
+                in 17..22 -> {
+                    when (i) {
+                        0 -> rootView.tab_layout.getTabAt(i)!!.text = "Evening"
+                        1 -> rootView.tab_layout.getTabAt(i)!!.text = "Night"
+                        2 -> rootView.tab_layout.getTabAt(i)!!.text = "Morning"
+                        3 -> rootView.tab_layout.getTabAt(i)!!.text = "Day"
+                    }
+                }
+                else -> {
+                    when (i) {
+                        0 -> rootView.tab_layout.getTabAt(i)!!.text = "Night"
+                        1 -> rootView.tab_layout.getTabAt(i)!!.text = "Morning"
+                        2 -> rootView.tab_layout.getTabAt(i)!!.text = "Day"
+                        3 -> rootView.tab_layout.getTabAt(i)!!.text = "Evening"
+
+                    }
+                }
             }
         }
 
--- app/src/main/java/com/example/weatherforecastapp/MasterFragment.kt
+++ app/src/main/java/com/example/weatherforecastapp/MasterFragment.kt
@@ -19,17 +19,24 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MasterFragment : Fragment() {
     lateinit var recyclerAdapter: WeatherRecyclerAdapter
     val args by navArgs<MasterFragmentArgs>()
     var recyclerView: RecyclerView? = null
+    var constraintLayout: ConstraintLayout? = null
+    val calendar = Calendar.getInstance()
 
     @SuppressLint("SetTextI18n")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_master, container, false)
         recyclerView = rootView.findViewById(R.id.recyclerview) as RecyclerView
+        constraintLayout = rootView.findViewById(R.id.constraint) as ConstraintLayout
+        setBackground(constraintLayout!!)
         getWeatherDetail()
+        Log.i("ArtREQUEST", "RESPONSE: getWeatherDetail()  ${getWeatherDetail()}")
         return rootView
     }
 
@@ -41,12 +48,8 @@ class MasterFragment : Fragment() {
             try {
                 withContext(Dispatchers.Main) {
                     if (response.isSuccessful) {
-                        response.body()?.let {
-                            initRecyclerView(it)
-//                            setSynteticBackground(it)
-                        }
-
-                        Log.i("ArtREQUEST", "RESPONSE: getWeatherDetail()  ${response.body()!!.listWeather!!.get(0).date!!}")
+                        response.body()?.let { initRecyclerView(it) }
+//                        Log.i("ArtREQUEST", "RESPONSE: getWeatherDetail()  ${response.body()!!.listWeather!!.get(0).date!!}")
 
                     } else {
                         Log.e("ArtREQUEST", "Error network operation failed with ${response.code()}")
@@ -58,29 +61,28 @@ class MasterFragment : Fragment() {
                 Log.e("ArtREQUEST", "Ooops: Something else went wrong")
             }
         }
+
     }
 
     private fun initRecyclerView(modelWeather: ModelWeather) {
-//        city_text.text = modelWeather.city!!.city
+        city_text.text = modelWeather.city!!.city
         val weatherItems = ArrayList<WeatherItem>()
         for (i in 0 until 8) {
-            weatherItems.add(WeatherItem(modelWeather.city!!.city,
+            weatherItems.add(WeatherItem(modelWeather.city.city,
                     Utils().FormatTemp(modelWeather.listWeather!!.get(i).main!!.temp.toString()),
                     modelWeather.listWeather.get(i).weather!!.get(0).description!!.capitalize(),
                     modelWeather.listWeather.get(i).weather!!.get(0).icon,
                     modelWeather.listWeather.get(i).date,
-                    modelWeather.listWeather.get(i).wind!!.speed,
-                    modelWeather.listWeather.get(i).clouds!!.clouds,
+                    modelWeather.listWeather.get(i).wind!!.speed + "ms",
+                    modelWeather.listWeather.get(i).clouds!!.clouds + "%",
                     Utils().FormatTemp(modelWeather.listWeather.get(i).main!!.feels_like.toString()),
-                    modelWeather.listWeather.get(i).main!!.humidity,
+                    modelWeather.listWeather.get(i).main!!.humidity + "%",
                     modelWeather.city.listCoordinates!!.latitude,
                     modelWeather.city.listCoordinates.longitude))
         }
         recyclerAdapter = WeatherRecyclerAdapter(weatherItems, args.tabNumber)
 
-        setBackground(modelWeather)
         recyclerView!!.apply {
-            setBackgroundResource(setBackground(modelWeather))
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(requireActivity().applicationContext, LinearLayoutManager.VERTICAL, false)
             adapter = recyclerAdapter
@@ -88,34 +90,76 @@ class MasterFragment : Fragment() {
 
     }
 
-    private fun setBackground(modelWeather: ModelWeather): Int {
-        val time = modelWeather.listWeather!!.get(0).date!!.substring(11)
-        return when (time) {
-            "00:00:00" ->  R.drawable.night_bg
-            "03:00:00" ->  R.drawable.night_morning_gradient_gb
-            "06:00:00" ->  R.drawable.morning_bg
-            "09:00:00" ->  R.drawable.morning_day_gradient_bg
-            "12:00:00" ->  R.drawable.day_bg
-            "15:00:00" ->  R.drawable.day_evening_gradient_bg
-            "18:00:00" ->  R.drawable.evening_bg
-            "21:00:00" ->  R.drawable.evening_night_gradient_bg
-            else ->  R.drawable.night_bg
+    private fun setBackground(constraint: ConstraintLayout) {
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val tabNumber = args.tabNumber
+        when (hour) {
+            in 0..2 -> {
+                when (tabNumber) {
+                    1 -> constraint.setBackgroundResource(R.drawable.night_bg)
+                    2 -> constraint.setBackgroundResource(R.drawable.morning_bg)
+                    3 -> constraint.setBackgroundResource(R.drawable.day_bg)
+                    4 -> constraint.setBackgroundResource(R.drawable.evening_bg)
+                }
+            }
+            in 3..5 -> {
+                when (tabNumber) {
+                    1 -> constraint.setBackgroundResource(R.drawable.night_morning_gradient_gb)
+                    2 -> constraint.setBackgroundResource(R.drawable.morning_day_gradient_bg)
+                    3 -> constraint.setBackgroundResource(R.drawable.day_evening_gradient_bg)
+                    4 -> constraint.setBackgroundResource(R.drawable.evening_night_gradient_bg)
+                }
+            }
+            in 6..8 -> {
+                when (tabNumber) {
+                    1 -> constraint.setBackgroundResource(R.drawable.morning_bg)
+                    2 -> constraint.setBackgroundResource(R.drawable.day_bg)
+                    3 -> constraint.setBackgroundResource(R.drawable.evening_bg)
+                    4 -> constraint.setBackgroundResource(R.drawable.night_bg)
+                }
+            }
+            in 9..11 -> {
+                when (tabNumber) {
+                    1 -> constraint.setBackgroundResource(R.drawable.morning_day_gradient_bg)
+                    2 -> constraint.setBackgroundResource(R.drawable.day_evening_gradient_bg)
+                    3 -> constraint.setBackgroundResource(R.drawable.evening_night_gradient_bg)
+                    4 -> constraint.setBackgroundResource(R.drawable.night_morning_gradient_gb)
+                }
+            }
+            in 12..14 -> {
+                when (tabNumber) {
+                    1 -> constraint.setBackgroundResource(R.drawable.day_bg)
+                    2 -> constraint.setBackgroundResource(R.drawable.evening_bg)
+                    3 -> constraint.setBackgroundResource(R.drawable.night_bg)
+                    4 -> constraint.setBackgroundResource(R.drawable.morning_bg)
+                }
+            }
+            in 15..17 -> {
+                when (tabNumber) {
+                    1 -> constraint.setBackgroundResource(R.drawable.day_evening_gradient_bg)
+                    2 -> constraint.setBackgroundResource(R.drawable.evening_night_gradient_bg)
+                    3 -> constraint.setBackgroundResource(R.drawable.night_morning_gradient_gb)
+                    4 -> constraint.setBackgroundResource(R.drawable.morning_day_gradient_bg)
+                }
+            }
+            in 18..20 -> {
+                when (tabNumber) {
+                    1 -> constraint.setBackgroundResource(R.drawable.evening_bg)
+                    2 -> constraint.setBackgroundResource(R.drawable.night_bg)
+                    3 -> constraint.setBackgroundResource(R.drawable.morning_bg)
+                    4 -> constraint.setBackgroundResource(R.drawable.day_bg)
+                }
+            }
+            in 21..23 -> {
+                when (tabNumber) {
+                    1 -> constraint.setBackgroundResource(R.drawable.evening_night_gradient_bg)
+                    2 -> constraint.setBackgroundResource(R.drawable.night_morning_gradient_gb)
+                    3 -> constraint.setBackgroundResource(R.drawable.morning_day_gradient_bg)
+                    4 -> constraint.setBackgroundResource(R.drawable.day_evening_gradient_bg)
+                }
+            }
         }
-    }
 
-    private fun setSynteticBackground(modelWeather: ModelWeather) {
-        val time = modelWeather.listWeather!!.get(0).date!!.substring(11)
-        when (time) {
-            "00:00:00" -> recyclerview!!.setBackgroundResource(R.drawable.night_bg)
-            "03:00:00" -> recyclerview!!.setBackgroundResource(R.drawable.night_morning_gradient_gb)
-            "06:00:00" -> recyclerview!!.setBackgroundResource(R.drawable.morning_bg)
-            "09:00:00" -> recyclerview!!.setBackgroundResource(R.drawable.morning_day_gradient_bg)
-            "12:00:00" -> recyclerview!!.setBackgroundResource(R.drawable.day_bg)
-            "15:00:00" -> recyclerview!!.setBackgroundResource(R.drawable.day_evening_gradient_bg)
-            "18:00:00" -> recyclerview!!.setBackgroundResource(R.drawable.evening_bg)
-            "21:00:00" -> recyclerview!!.setBackgroundResource(R.drawable.evening_night_gradient_bg)
-
-        }
     }
 
 }
